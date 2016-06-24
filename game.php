3e256067e483bc522b3d1e68751988a4d26f4a98
Ishaan Puniani <ishaan.puniani@gmail.com>
2016-06-25 00:22:43 +0200

    dimension fix

---
+++ game.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>GOL | Jack Or Better</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./css/reset.css" type="text/css">
+        <link rel="stylesheet" href="./css/main.css" type="text/css">
+
+
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,minimal-ui">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="msapplication-tap-highlight" content="no">
+	    <script>
+            var GAMEID = "JackOrBetter_Server";
+        </script>
+
+        <script type="text/javascript" src="./dist/production0.1.0.min.js"></script>
+    </head>
+    <body ondragstart="return false;" ondrop="return false;">
+	   
+        <canvas id="canvas" class="ani_hack" width="1920 " height="768"> </canvas>
+        <div id="block_game" style="position: fixed; background-color: transparent; top: 0px; left: 0px; width: 100%; height: 100%; display:none"></div>
+<script>
+$(document).ready(function(){
+	init();
+})
+</script>
+    </body>
+</html>
